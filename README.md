fcf41047dbac8326db7c19fe970f1f6fdaca350e
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-08-14 18:51:12 +0900

    add gh-pages for example

---
+++ README.md
@@ -0,0 +1,2 @@
+# mbgl-tilecloud-control
+
---
+++ docs/src/app.js
@@ -0,0 +1,11 @@
+import tcControl from '../../src/app'
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'https://tilecloud.github.io/tiny-tileserver/style.json',
+  attributionControl: true,
+  hash: true
+});
+map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.GeolocateControl());
+map.addControl(new tcControl(), 'bottom-left');
